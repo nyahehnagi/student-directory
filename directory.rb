a4db76814094345c6e85b31684226743ab24cafa
Bromley <bromley.jones@gmail.com>
2022-01-05 13:26:29 +0000

    added user input to add names and cohort

--- directory.rb
+++ directory.rb
@@ -1,18 +1,4 @@
 # put students into an array of hashes
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -29,6 +15,28 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students and cohort"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    puts "Enter cohort"
+    cohort = gets.chomp
+    # add the student hash to the array
+    students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
