1fbf6a186ba39cf4c0264d65b80e7fe7a529dd49
Bromley <bromley.jones@gmail.com>
2022-01-05 14:08:55 +0000

    exercise 4, using 'until' as method of looping

---
+++ directory_exercise4.rb
@@ -0,0 +1,43 @@
+# put students into an array of hashes
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  counter = 0
+  until counter == students.count 
+    puts "#{students[counter][:name]}"
+    counter += 1
+  end
+end
+
+def print_footer(names)
+  # print the number of students
+  puts "Overall, we have #{names.count} great students"
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} student(s)"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
+
