4b504f85383046001f0f1d54b192856f7679a4d6
Bromley <bromley.jones@gmail.com>
2022-01-05 20:46:31 +0000

    added saving of data to file

--- directory_interactive.rb
+++ directory_interactive.rb
@@ -46,6 +46,7 @@ def print_menu
     # 1. print the menu and ask the user what to do
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -61,11 +62,25 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
     puts "I don't know what you meant, try again"
   end
+end
+
+def save_students
+  # open file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def interactive_menu
