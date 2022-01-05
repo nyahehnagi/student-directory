22eca27bb7badee06801b8ce0e8c4d7bc2ce0311
Bromley <bromley.jones@gmail.com>
2022-01-05 13:56:56 +0000

    exercise 3,  printing of names of certain length

---
+++ directory_exercise3.rb
@@ -0,0 +1,70 @@
+class String
+  def initial
+    self.chr
+  end
+end
+
+# put students into an array of hashes
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each_with_index do |student, idx|
+    puts "#{idx + 1} #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_with_letter(students)
+  puts "Please enter a letter"
+  letter = gets.chomp.upcase 
+  students.each do |student|
+    if student[:name].initial.upcase == letter
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_names_shorter_than(students)
+  puts "Please enter max length for a name"
+  length = gets.chomp.to_i 
+  students.each do |student|
+    if student[:name].length <= length
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_footer(names)
+  # print the number of students
+  puts "Overall, we have #{names.count} great students"
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} student(s)"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
+
+print_names_shorter_than(students)
+
+
