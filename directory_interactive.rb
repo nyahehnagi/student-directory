6e938ca44bc54c0fbcb61c12d7b03f70344e5b19
Bromley <bromley.jones@gmail.com>
2022-01-05 20:36:01 +0000

    refactor - added method for process and renaming

--- directory_interactive.rb
+++ directory_interactive.rb
@@ -5,7 +5,7 @@ def print_header
   puts "-------------"
 end
 
-def print
+def print_students_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
@@ -51,26 +51,28 @@ end
 
 def show_students
   print_header
-  print
+  print_students_list
   print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
 def interactive_menu
   loop do
     print_menu
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    # read the input and process 
+    process(gets.chomp)
   end
 end
 
