adc1eaebb3be8da4e7b1a2b723701438427c93b8
Bromley <bromley.jones@gmail.com>
2022-01-05 13:37:42 +0000

    exercise 1, using each_with_index

---
+++ directory_exercise1.rb
@@ -0,0 +1,41 @@
+# put students into an array of hashes
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each_with_index do |student, idx|
+    puts "#{idx + 1} #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  # print the number of students
+  puts "Overall, we have #{names.count} great students"
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} student(s)"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
+
