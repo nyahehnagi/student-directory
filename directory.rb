ac8e8bf1c0b06fdfcfecf7134af518c090ddcc42
Bromley <bromley.jones@gmail.com>
2022-01-05 13:13:16 +0000

    refactor to use methods for printing

--- directory.rb
+++ directory.rb
@@ -13,10 +13,23 @@ students = [
   "Norman Bates"
 ]
 
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-# print the number of students
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  # print the number of students
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
+
